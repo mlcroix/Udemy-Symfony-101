9b0ce31ee334eea7c4bfbcbb591f42ac3bbb7fcf
Michel du Croix <michelducroix@gmail.com>
2026-06-18 11:58:02 +0200

    Add default views - add views for showing messages

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,12 +2,38 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
-    public function index(): Response
+    private $messages = [
+        "meep",
+        "moop",
+        "maap"
+    ];
+
+    #[Route('/{limit?}', name: 'app_index', requirements: ['limit' => '\d+'])]
+    public function index(?int $limit = 3): Response
     {
-        return new Response('Hello, World!');
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' => $limit
+            ]
+        );
+    }
+
+    #[Route('/message/{id}', name: "app_show_one", requirements: ['id' => '\d+'])]
+    public function showOne(int $id): Response {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
     }
 }
