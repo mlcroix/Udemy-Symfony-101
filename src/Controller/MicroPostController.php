4f839fcac7e125cb67e98e71e75bb7397aaa615e
Michel du Croix <michelducroix@gmail.com>
2026-06-24 10:14:13 +0200

    Create  Add post endpoint - create a form type - create new add post view - create new endpoint for add post

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -7,8 +7,10 @@ use App\Repository\MicroPostRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\MicroPostType;
 
 final class MicroPostController extends AbstractController
 {
@@ -36,6 +38,32 @@ final class MicroPostController extends AbstractController
             'posts' => $microPosts
 
         ]);
+    }
+
+    #[Route('/micro-post/add', name: "app_micro_post_add")]
+    public function add(Request $request): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createForm(MicroPostType::class, $microPost);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $microPost->setCreated(new DateTime());
+            
+            $this->entityManagerInterface->persist($microPost);
+            $this->entityManagerInterface->flush();
+            
+            $this->addFlash('success', 'Post created successfully!');
+            
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->render('micro_post/add.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
     }
 
     #[Route('/micro-post/{post}', name: "app_micro_post_show")]
---
+++ src/Form/MicroPostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MicroPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MicroPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => 'Content',
+                'attr' => ['class' => 'form-control', 'rows' => 5]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save Post',
+                'attr' => ['class' => 'btn btn-primary mt-3']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MicroPost::class,
+        ]);
+    }
+}
